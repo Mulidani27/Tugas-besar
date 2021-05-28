db905d6504e65ef60aa110d1dfcb2b0a7c53b2ce
khairin04 <khairinsajida00@gmail.com>
2021-05-28 13:59:36 +0700

    try

---
+++ praktikum1.php
@@ -0,0 +1,11 @@
+<table border='1' cellpading='10' cellspasing='0'>
+<?php 
+	for($i = 1; $i <= 3; $i++){ 
+			echo "<tr>"; 
+			for($j = 1; $j <= 5; $j++){ 
+					echo "<td>$i , $j</td>"; 
+			} 
+			echo "</tr>"; 
+			} 
+ 
+ ?> 
